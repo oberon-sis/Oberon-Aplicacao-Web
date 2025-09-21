94e041800ac180726527a2234a1507eabf4a1fb9
JhoelDiego2 <jhoel.mita@sptech.school>
2025-09-20 22:12:01 -0300

    feat: Adicionado README, estrutura inicial de pastas e dependências

---
+++ public/js/sessao.js
@@ -0,0 +1,11 @@
+function validarSessao() {
+  var usuario = sessionStorage.ID_USUARIO;
+  if (usuario == null ) {
+    window.location = "../login.html";
+  }
+}
+
+function limparSessao() {
+  sessionStorage.clear();
+  window.location = "../login.html";
+}
