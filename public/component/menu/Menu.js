00fc89bfa0a4ca602781e0e1360d6a4a56d2602e
nathanbarbosatx <nathan.teixeira@sptech.school>
2025-10-28 14:50:34 -0300

    chore: Adiciona os aquivos eliminados no commit anterior

--- public/component/menu/Menu.js
+++ public/component/menu/Menu.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const idUsuario = sessionStorage.ID_USUARIO ? sessionStorage.ID_USUARIO : 5;
 
-  fetch('/component/menu/Menu.html')
+  fetch('../component/menu/MenuEsqueleto.html')
     .then((response) => {
       if (!response.ok) {
         throw new Error('Erro ao carregar o esqueleto do menu.');
@@ -11,23 +11,23 @@ document.addEventListener('DOMContentLoaded', function () {
     .then((html) => {
       document.getElementById('menu-container').innerHTML = html;
 
-      return fetch(`/menu/getMenu/${idUsuario}`);
+      // return fetch(`/menu/getMenu/${idUsuario}`);
     })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Erro ao carregar os dados do menu do backend.');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      document.getElementById('painelPC').innerHTML += data.painelPC;
-      document.getElementById('alertaSuportePC').innerHTML += data.alertaSuportePC;
-      document.getElementById('gestaoAreaPC').innerHTML += data.gestaoAreaPC;
+    // .then(response => {
+    //     if (!response.ok) {
+    //         throw new Error('Erro ao carregar os dados do menu do backend.');
+    //     }
+    //     return response.json();
+    // })
+    // .then(data => {
+    //     document.getElementById('painelPC').innerHTML += data.painelPC;
+    //     document.getElementById('alertaSuportePC').innerHTML += data.alertaSuportePC;
+    //     document.getElementById('gestaoAreaPC').innerHTML += data.gestaoAreaPC;
 
-      document.getElementById('painelMobile').innerHTML += data.painelMobile;
-      document.getElementById('gestaoAreaMobile').innerHTML += data.gestaoAreaMobile;
-      document.getElementById('alertaSuporteMobile').innerHTML += data.alertaSuporteMobile;
-    })
+    //     document.getElementById('painelMobile').innerHTML += data.painelMobile;
+    //     document.getElementById('gestaoAreaMobile').innerHTML += data.gestaoAreaMobile;
+    //     document.getElementById('alertaSuporteMobile').innerHTML += data.alertaSuporteMobile;
+    // })
     .catch((error) => {
       console.error('Falha ao carregar o menu:', error);
       const menuContainer = document.getElementById('menu-container');
