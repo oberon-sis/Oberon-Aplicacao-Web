01ffafc9511ff639814b9bb9674991c6f580ded5
d1n4ara <dandararamoscosta@gmail.com>
2025-10-30 11:32:14 -0300

    (

--- src/models/gerenciamentoUsuarioModel.js
+++ src/models/gerenciamentoUsuarioModel.js
@@ -55,7 +55,7 @@ function PesquisarUsuario(campo, valor) {
             f.nome, 
             f.cpf, 
             f.email, 
-            t.tipoUsuario AS funcao
+            TipoUsuario.tipoUsuario AS funcao
         FROM Funcionario AS f
         JOIN TipoUsuario AS t ON f.fkTipoUsuario = t.idTipoUsuario
       WHERE ${campo} LIKE '${valor}%';
