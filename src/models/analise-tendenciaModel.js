994ef80e92c21eac3d5f55bee1a344302051b959
JhoelDiego2 <jhoel.mita@sptech.school>
2025-11-22 20:38:23 -0300

    feat: adiciona extração dos dados da tabela e faz o tratamento delas no back end para o retorno de uma json limpa para o frotn (paramettros no model mocados para teste)

--- src/models/analise-tendenciaModel.js
+++ src/models/analise-tendenciaModel.js
@@ -12,7 +12,7 @@ function buscar_total_alertas_e_criticos_atual_e_passado(dataInicio, dataFim, id
 function buscar_uptime_atual_e_passado(dataInicio, dataFim, idEmpresa, idMaquina) {
   console.log('[MODEL] - Buscando uptime total atual e passado:', dataInicio, dataFim, idEmpresa, idMaquina);
 
-  var instrucaoSql = ` CALL sp_uptime_intervalo('2025-10-01 00:00:00', NOW(), 6, NULL);`;
+  var instrucaoSql = `CALL sp_uptime_intervalo('2025-10-06 00:00:00', NOW(), 6, NULL);`;
   console.log('Executando a instrução SQL: \n' + instrucaoSql);
 
   return database.executar(instrucaoSql);
@@ -31,7 +31,7 @@ function buscar_alerta_moda_e_total(dataInicio, dataFim, idEmpresa, idMaquina) {
 function buscar_maquinas(idEmpresa) {
   console.log('[MODEL] - Buscando maquinas da idEmpresa:', idEmpresa);
 
-  var instrucaoSql = `SELECT idMaquina, nome FROM Maquinas WHERE fkEmpresa = ${idEmpresa}`;
+  var instrucaoSql = `SELECT idMaquina, nome FROM Maquina WHERE fkEmpresa = ${idEmpresa}`;
     
   console.log('Executando a instrução SQL: \n' + instrucaoSql);
 
@@ -49,6 +49,17 @@ function buscar_dados_brutos_atual_e_passado(dataInicio, dataFim, idEmpresa, idM
   return database.executar(instrucaoSql);
 }
 
+function buscar_ranking_tabelas_desempenho(dataInicio, dataFim, idEmpresa, limite) {
+  console.log('[MODEL] - Buscando maquina mais critica:', dataInicio, dataFim, idEmpresa, limite);
+
+  var instrucaoSql = `CALL sp_dados_tabela('2025-11-10 00:00:00', NOW(), 6, 3);`;
+    
+  console.log('Executando a instrução SQL: \n' + instrucaoSql);
+
+  return database.executar(instrucaoSql);
+}
+
+
 
 module.exports = {
 buscar_uptime_atual_e_passado,
@@ -56,4 +67,5 @@ buscar_total_alertas_e_criticos_atual_e_passado,
 buscar_alerta_moda_e_total,
 buscar_dados_brutos_atual_e_passado,
 buscar_maquinas, 
+buscar_ranking_tabelas_desempenho, 
 };
