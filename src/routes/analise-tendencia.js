e59b6cbd768ee87e670e6a2912fdf29958536226
JhoelDiego2 <jhoel.mita@sptech.school>
2025-11-23 18:15:49 -0300

    feat: redirecionamento para a analisis no python

--- src/routes/analise-tendencia.js
+++ src/routes/analise-tendencia.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var analiseController = require('../controllers/analise-tendenciaController');
+var IaController = require('../controllers/analisisIAController')
 
 router.get('/procurar_dados_pagina', function (req, res) {
   analiseController.procurar_dados_pagina(req, res);
@@ -9,6 +10,10 @@ router.get('/procurar_dados_pagina', function (req, res) {
 
 router.get('/procurar_maquinas', function (req, res) {
   analiseController.procurar_maquinas(req, res);
+});
+
+router.post('/grafico', function (req, res) {
+    IaController.buscarDadosGrafico(req, res);
 });
 
 module.exports = router;
