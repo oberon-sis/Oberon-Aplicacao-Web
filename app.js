fa97a52f5b88e7354deacddf09711a94bd5098f5
nathanbarbosatx <nathan.teixeira@sptech.school>
2025-11-27 10:39:05 -0300

    feat: organização da preparação do controller para o painel especifico

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ var empresaRouter = require('./src/routes/empresas');
 var authRouter = require('./src/routes/email');
 var alertasRouter = require('./src/routes/alertas');
 const downloadRoutes = require('./src/routes/appInstalacao');
-const painelRoutes = require('./src/routes/painel');
+const painelRoutes = require('./src/routes/dashboardEspecifica');
+
+const dashboardEspecificaRouter = require('./src/routes/dashboardEspecifica'); 
+
 
 // --- CONFIGURAÇÃO DOS MIDDLEWARES ---
 app.use(express.json());
@@ -46,7 +49,8 @@ app.use('/maquinas', maquinasRouter);
 app.use('/auth', authRouter);
 app.use('/alertas', alertasRouter);
 app.use('/api/download', downloadRoutes);
-app.use('/painel', painelRoutes);
+
+app.use('/dashboardEspecifica', dashboardEspecificaRouter);
 
 app.listen(PORTA_APP, function () {
   console.log(`                                                                            
