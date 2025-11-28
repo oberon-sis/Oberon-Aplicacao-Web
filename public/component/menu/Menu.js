ea83184e806850d8dd724e2a213a117b5ba45969
nathanbarbosatx <nathan.teixeira@sptech.school>
2025-11-28 11:27:23 -0300

    feat: adiciona o espaço no menu para a dash indivisual

--- public/component/menu/Menu.js
+++ public/component/menu/Menu.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   const DASHBOARD_MAP = {
     ver_dash_monitoramento: { nome: 'Monitoramento Ativo', url: 'painel.html' },
-    ver_dash_historico: { nome: 'Análise Histórica', url: 'painelEspecifico.html' },
+    ver_dash_nathan: { nome: 'Análise Historica', url: 'analiseHistorica.html' },
     ver_dash_pedro: { nome: 'Análise de Parâmetros', url: 'dashPedro.html' },
     ver_dash_jhoel: { nome: 'Análise de Desempenho', url: 'análiseDesempenho.html' },
     ver_dash_dandara: { nome: 'Gestão de Risco', url: 'dashboardRiscoOperacional.html' },
     ver_dash_miguel: { nome: 'Gestão de Ativos', url: 'DashboardMiguel.html' },
     ver_dash_bruna: { nome: 'Risco de Componentes', url: 'riscoComponente.html' },
-    ver_dash_nathan: { nome: 'Dashboard Nathan', url: 'dashNathan.html' },
   };
 
   function ocultarElementoSeNaoPermitido(id, isPermitido) {
